94869d3c197fb80c467c7b8177c44f5f8882c753
pro-frontend <dzmitryj.ivchenkov@gmail.com>
2021-06-29 18:17:19 +0300

    fix(ts7016): add @types modules and path for tsconfig file

--- src/screens/Root/Root.tsx
+++ src/screens/Root/Root.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 const Root = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Title>This is Entry Point.</Title>
       <StatusBar />
     </View>
   );
 };
+
+const Title = styled.Text`
+  color: #000;
+  text-align: center;
+  font-size: 16px;
+`;
 
 const styles = StyleSheet.create({
   container: {
